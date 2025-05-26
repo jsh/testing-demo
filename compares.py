381ea48cde626947a309affc3eb4169676471189
jeffrey_haemer <jeffrey.haemer@gmail.com>
2025-05-26 17:16:13 +0000

    initial

---
+++ compares.py
@@ -0,0 +1,16 @@
+"""Comparison Functions"""
+
+
+def my_gt(a, b):
+    """My greater-than."""
+    return a > b
+
+
+def my_lt(a, b):
+    """My less-than."""
+    return a < b
+
+
+def my_eq(a, b):
+    """My equals."""
+    return a == b
